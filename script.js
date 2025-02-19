3873e187c8f6d08922912869ae7534a24f71721c
acquila91 <albertoacquilino@gmail.com>
2025-02-19 12:18:56 -0300

    modified image input

--- script.js
+++ script.js
@@ -2,3 +2,7 @@ function toggleMobileMenu() {
   const navElement = document.querySelector("nav");
   navElement.classList.toggle("show-mobile");
 }
+
+function toggleImageSize(image) {
+  image.classList.toggle("expanded");
+}
