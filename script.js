e4250a0d767518c590272040a22a7892310c1d66
acquila91 <albertoacquilino@gmail.com>
2025-02-19 22:20:15 -0300

    updated portfolio

--- script.js
+++ script.js
@@ -6,28 +6,3 @@ function toggleMobileMenu() {
 function toggleImageSize(image) {
   image.classList.toggle("expanded");
 }
-
-function openFullscreen(img) {
-  // Create an overlay
-  let overlay = document.createElement("div");
-  overlay.classList.add("overlay");
-  overlay.onclick = closeFullscreen;
-
-  // Clone the clicked image and make it fullscreen
-  let fullscreenImg = img.cloneNode(true);
-  fullscreenImg.classList.add("fullscreen");
-  fullscreenImg.onclick = closeFullscreen; // Click again to close
-
-  // Append elements to body
-  document.body.appendChild(overlay);
-  document.body.appendChild(fullscreenImg);
-
-  // Show overlay
-  overlay.style.display = "block";
-}
-
-function closeFullscreen() {
-  // Remove the fullscreen image and overlay
-  document.querySelector(".fullscreen").remove();
-  document.querySelector(".overlay").remove();
-}
