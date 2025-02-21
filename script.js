e19b557ea557c23912d99a06059019b3804a2232
acquila91 <albertoacquilino@gmail.com>
2025-02-20 21:18:31 -0300

    added input video gif

--- script.js
+++ script.js
@@ -6,3 +6,17 @@ function toggleMobileMenu() {
 function toggleImageSize(image) {
   image.classList.toggle("expanded");
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+  const video = document.querySelector(".input-video video");
+  let playCount = 0;
+
+  video.addEventListener("ended", function () {
+      playCount++;
+      if (playCount >= 3) { // Stops after 3 loops
+          video.pause();
+      } else {
+          video.play();
+      }
+  });
+});
