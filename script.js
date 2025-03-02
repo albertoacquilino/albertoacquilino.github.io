39076ae98abd0d45cdc672f7e15dbea617d623a4
acquila91 <albertoacquilino@gmail.com>
2025-03-02 16:02:20 -0300

    bug menu fix

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-function toggleMobileMenu() {
-  const navElement = document.querySelector("nav");
-  navElement.classList.toggle("show-mobile");
-}
-
 function toggleImageSize(image) {
   image.classList.toggle("expanded");
 }
